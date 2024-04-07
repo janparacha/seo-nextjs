2427235f10bccaeea372c7b5b916159371ea3ffe
janparacha <parmusta@gmail.com>
2024-04-07 19:08:07 +0200

    init

---
+++ app/articles/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terrain uno",
+  description: "A blog about ground and confusion.",
+};
+
+export default function Articles() {
+  return (
+    <main>
+      <div>
+        <h1>Articles numero uno</h1>
+      </div>
+    </main>
+  );
+}
---
+++ app/articles/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terrain entrant",
+  description: "A blog about ground and confusion.",
+};
+
+export default function Articles() {
+  return (
+    <main>
+      <div>
+        <h1>Articles </h1>
+      </div>
+    </main>
+  );
+}
